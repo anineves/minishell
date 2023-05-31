b955249e6fbe2a1eb7dafde7967eb8078e204230
Ana Neves <anineves5@gmail.com>
2023-05-31 19:43:50 +0100

    export

--- builtins/export.c
+++ builtins/export.c
@@ -1,5 +1,27 @@
 #include "../minishell.h"
 
+void	ft_quotes(char *str, t_global *global)
+{
+	int	i;
+	int	equal;
+
+	i = 0;
+	equal = 0;
+	while (str[i])
+	{
+		if (str[i] == '=' && equal == 0)
+		{
+			equal = 1;
+			ft_putchar_fd(str[i], global->fd_output);
+			ft_putchar_fd('"', global->fd_output);
+		}
+		else
+			ft_putchar_fd(str[i], global->fd_output);
+		i++;
+	}
+	ft_putchar_fd('"', global->fd_output);
+}
+
 void	ft_sort(t_global *global)
 {
 	int		sort;
@@ -26,9 +48,30 @@ void	ft_sort(t_global *global)
 	}
 }
 
+void	verific_equal(char *copy_env, t_global *global)
+{
+	int	i;
+	int	equal;
+	
+	i = 0;
+	equal = 0;
+	
+	while(copy_env[i])
+	{
+		if(copy_env[i] == '=')
+			equal = 1;
+		i++;
+	}
+	if(equal == 1)
+		ft_quotes(copy_env, global);
+	else
+		ft_putstr_fd(copy_env, global->fd_output);
+	ft_putchar_fd('\n', global->fd_output);
+}
+
 void ft_export(t_global *global)
 {
-	int i;
+	int	i;
 	
 	i = 0;
 	if(global->args[1] == NULL)
@@ -37,10 +80,8 @@ void ft_export(t_global *global)
 		while (global->copy_env[i])
 		{
 			ft_putstr_fd("declare -x ", 1);
-				ft_putstr_fd(global->copy_env[i], global->fd_output);
-				i++;
-			ft_putchar_fd('\n', global->fd_output);
+			verific_equal(global->copy_env[i], global);			
+			i++;
 		}
-	}
-	//apenas se nao tiver argumentos
+	}	//apenas se nao tiver argumentos
 }
