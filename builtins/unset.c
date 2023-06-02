1500a52ed295ac1c2d262042c76bb63039be684f
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-06-02 15:48:00 +0100

    Update unset.c

--- builtins/unset.c
+++ builtins/unset.c
@@ -22,11 +22,12 @@ void ft_unset(t_global *global)
 	int 	i;
 	int	len;
 	int	j;
+	int k;
 	
 	i = 1;
 	len = 0;
 	j = 0;
-	
+	k = 0;
 	if(!global->args[1])
 		g_exit_status = 0;
 	else
@@ -34,12 +35,17 @@ void ft_unset(t_global *global)
 		while(global->args[i])
 		{
 			len = ft_strlen(global->args[i]);
+			while(global->args[i][k] && global->args[i][k] != '=')
+				k++;
 			while(global->copy_env[j])	
 			{
 				if(!ft_strncmp(global->args[i], global->copy_env[j], len))
 					rmv_var(global, j);
+				else if(!ft_strncmp(global->args[i], global->copy_env[j], k))
+					rmv_var(global, j);
 				j++;
 			}
+			j=0;
 			i++;
 			free(global->args[i]);
 		}
