802da1d082390618965b86739c41887088e87429
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-06-03 11:27:59 +0100

    Update unset.c

--- builtins/unset.c
+++ builtins/unset.c
@@ -2,18 +2,22 @@
 
 extern int	g_exit_status;
 
-void	rmv_var(t_global *global, int j)
+char	**rmv_var(t_global *global, int j,  int size)
 {
-	int i;
-	
-	i = j;
-	while(global->copy_env[i])
-	{
-		global->copy_env[i] = global->copy_env[i + 1];
-		i++;
-	}
-	global->copy_env[i] = NULL;
+	int			i;
+	char		**new_copy_env;
 
+	i = 0;
+	new_copy_env = ft_calloc(size + 1 , sizeof(char *));
+	while (i < j)
+	{
+		new_copy_env[i] = ft_strdup(global->copy_env[i]);
+		(global->copy_env != NULL)
+	{
+		free_args(global->copy_env);
+		free(global->copy_env);
+	}
+	return (new_copy_env);
 }
 
 
@@ -24,9 +28,7 @@ void ft_unset(t_global *global)
 	int	j;
 	int k;
 	
-	i = 1;
-	len = 0;
-	j = 0;
+	i = 1;-fsanitize=address
 	k = 0;
 	if(!global->args[1])
 		g_exit_status = 0;
@@ -40,14 +42,59 @@ void ft_unset(t_global *global)
 			while(global->copy_env[j])	
 			{
 				if(!ft_strncmp(global->args[i], global->copy_env[j], len))
-					rmv_var(global, j);
+					global->copy_env = rmv_var(global, j, (size_env(global->copy_env) - 1));
 				else if(!ft_strncmp(global->args[i], global->copy_env[j], k))
-					rmv_var(global, j);
+					global->copy_env = rmv_var(global, j, (size_env(global->copy_env) - 1));
 				j++;
 			}
+			k=0;
 			j=0;
 			i++;
-			free(global->args[i]);
+		}
+	}
+}
+		new_copy_env[i] = ft_strdup(global->copy_env[i + 1]);
+		i++;
+	}
+	new_copy_env[i] = NULL;
+	if (global->copy_env != NULL)
+	{
+		free_args(global->copy_env);
+		free(global->copy_env);
+	}
+	return (new_copy_env);
+}
+
+
+void ft_unset(t_global *global)
+{
+	int 	i;
+	int	len;
+	int	j;
+	int k;
+	
+	i = 1;-fsanitize=address
+	k = 0;
+	if(!global->args[1])
+		g_exit_status = 0;
+	else
+	{
+		while(global->args[i])
+		{
+			len = ft_strlen(global->args[i]);
+			while(global->args[i][k] && global->args[i][k] != '=')
+				k++;
+			while(global->copy_env[j])	
+			{
+				if(!ft_strncmp(global->args[i], global->copy_env[j], len))
+					global->copy_env = rmv_var(global, j, (size_env(global->copy_env) - 1));
+				else if(!ft_strncmp(global->args[i], global->copy_env[j], k))
+					global->copy_env = rmv_var(global, j, (size_env(global->copy_env) - 1));
+				j++;
+			}
+			k=0;
+			j=0;
+			i++;
 		}
 	}
 }
