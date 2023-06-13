70ef3cff73488a877e5abdd1dbbdcea3feb37ab0
anineves <anineves5@gmail.com>
2023-06-13 18:01:29 +0100

    cat

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 11:03:45 by asousa-n          #+#    #+#             */
-/*   Updated: 2023/06/13 11:47:28 by asousa-n         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:56:41 by asousa-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 extern int	g_exit_status;
 
-void change_dir(t_global *global, char *next_path)
+void	change_dir(t_global *global, char *next_path)
 {
-	char *path;
-		
+	char	*path;
+
 	path = getcwd(NULL, 0);
 	global->old_path = path;
-	if (chdir(next_path) == 0 )
-			g_exit_status = 0;
+	if (chdir(next_path) == 0)
+		g_exit_status = 0;
 	else
 	{
 		printf("bash: cd: %s: No such file or directory\n", global->args[1]);
@@ -30,22 +30,22 @@ void change_dir(t_global *global, char *next_path)
 	free(path);
 }
 
-void ft_cd(t_global *global)
+void	ft_cd(t_global *global)
 {
-	char *next_path;
-	
+	char	*next_path;
+
 	if (global->shell->flag == HEREDOC || global->shell->flag == RD_IN)
 		return ;
 	next_path = NULL;
-	if(global->args[1] && global->args[2])
+	if (global->args[1] && global->args[2])
 	{
 		ft_putstr_fd("minishell: cd: too many arguments\n", global->fd_output);
-		g_exit_status = 1;		
+		g_exit_status = 1;
 		return ;
 	}
-	if( global->args[1] )
+	if (global->args[1])
 	{
-		if((ft_strcmp(global->args[1], "-") == 0))
+		if ((ft_strcmp(global->args[1], "-") == 0))
 		{
 			ft_putstr_fd(global->old_path, global->fd_output);
 			ft_putstr_fd("\n", global->fd_output);
@@ -54,7 +54,7 @@ void ft_cd(t_global *global)
 		}
 	}
 	next_path = global->args[1];
-	if((!global->args[1]) || (ft_strcmp(global->args[1], "~") == 0) || (ft_strncmp(global->args[1], "--", 3) == 0))
+	if ((!global->args[1]) || (ft_strcmp(global->args[1], "~") == 0))
 		next_path = getenv("HOME");
 	change_dir(global, next_path);
 }
